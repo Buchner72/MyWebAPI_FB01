43de35d56a125d84c3aa8dc4f138c65090fcd441
Buchner72 <franz.buchner@gmx.at>
2019-04-18 13:12:19 +0200

    Controller Add Personnan

---
+++ MyWebAPI/Controllers/PersonenController.cs
@@ -0,0 +1,51 @@
+﻿using MyWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace MyWebAPI.Controllers
+{
+    //  [Route("api/[controller]")]
+    public class PersonenController : ApiController
+    {
+        [HttpGet]
+        public IEnumerable<Person> Get()
+        {
+            return new[]
+            {
+                new Person
+                {
+                    Vorname = "Franz",
+                    Nachname ="Buchner",
+                    Id=1
+                }
+            };
+        }
+
+        public IHttpActionResult Get(int id)
+        {
+            Person p1 = new Person();
+            p1.Vorname = "Johann";
+            p1.Nachname = "Scheck";
+            return Ok(p1);
+            
+        }
+
+        [HttpPost]
+        public IHttpActionResult Post([FromBody] Person person)
+        {           
+            return Ok(person);
+        }
+
+        [HttpPut]
+        public IHttpActionResult Put(int id,[FromBody] Person person)
+        {
+            return Ok(person);
+        }
+
+
+    }
+}
---
+++ MyWebAPI/Models/Adresse.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace MyWebAPI.Models
+{
+    public class Adresse
+    {
+        public Adresse()
+        {
+            Personen = new HashSet<Person>();
+        }
+
+        public int Id { get; set; }
+        public string Strasse { get; set; }
+        public string Ort { get; set; }
+
+        public virtual ICollection<Person> Personen{ get; set; } 
+    }
+}
