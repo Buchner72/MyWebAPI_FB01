0c2ae53e60d298dc8e45ce8dc790fae83f58a142
Buchner72 <franz.buchner@gmx.at>
2019-05-04 12:52:29 +0200

    Wichtige Info

--- MyWebAPI/App_Start/WebApiConfig.cs
+++ MyWebAPI/App_Start/WebApiConfig.cs
@@ -12,7 +12,7 @@ namespace MyWebAPI
         public static void Register(HttpConfiguration config)
         {
 
-            //Enable CORS
+            // - Enable CORS (Wichtig!!  Damit Zugriff auch über localhost von Frontend aus funktioniert)
             config.EnableCors(new EnableCorsAttribute("http://localhost:4200", headers: "*", methods: "*"));
 
             // Web-API-Konfiguration und -Dienste
